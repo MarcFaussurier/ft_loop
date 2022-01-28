3de4b90c519435cb007c51b31103f728f40e7034
Marc Faussurier <faussurier.marc@icloud.com>
2022-01-28 16:52:09 +0000

    compiler pseudocode

---
+++ main.ç.compiler.c
@@ -0,0 +1,26 @@
+
+#ifndef MACRO_DEPTH
+# define MACRO_DEPTH 42
+#endif
+
+
+a fn (name, args)
+
+int main(int ac, char **av)
+{
+	int fd;
+
+	fd = open(av[1]);
+	tokens = [];
+
+	while (1)
+	{
+		token = get_token(source);
+		if (!token)
+			return ;
+		tokens[] = token;
+		if (tokens.length > 4 && tokens[len - x] .... )
+			a fn (token[len - x]) ....
+		
+	}
+}
